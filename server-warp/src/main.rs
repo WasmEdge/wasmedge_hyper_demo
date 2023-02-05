f830043d6c93891bd1e155f697fdeed46cee0803
Michael Yuan <michael@secondstate.io>
2023-02-05 11:22:02 -0600

    Add line breaks
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- server-warp/src/main.rs
+++ server-warp/src/main.rs
@@ -5,14 +5,14 @@ async fn main() {
     // GET /
     let help = warp::get()
         .and(warp::path::end())
-        .map(|| "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`");
+        .map(|| "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`\n");
 
     // POST /echo
     let echo = warp::post()
         .and(warp::path("echo"))
         .and(warp::body::bytes())
         .map(|body_bytes: bytes::Bytes| {
-            format!("{}", std::str::from_utf8(body_bytes.as_ref()).unwrap())
+            format!("{}\n", std::str::from_utf8(body_bytes.as_ref()).unwrap())
         });
 
     let routes = help.or(echo);
