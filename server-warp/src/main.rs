e4544d2a9e17f6cd50bc79259538ccef0f173298
csh <458761603@qq.com>
2024-05-08 16:33:22 +0800

    Change warp to axum
    Signed-off-by: csh <458761603@qq.com>

--- server-warp/src/main.rs
+++ server-warp/src/main.rs
@@ -1,20 +1,35 @@
-use warp::Filter;
+use bytes::Bytes;
+use futures_util::StreamExt;
+
+use axum::{extract::BodyStream, routing::get, routing::post, Router};
+use tokio::net::TcpListener;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // GET /
-    let help = warp::get()
-        .and(warp::path::end())
-        .map(|| "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`\n");
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(help))
+        .route("/echo", post(echo));
 
-    // POST /echo
-    let echo = warp::post()
-        .and(warp::path("echo"))
-        .and(warp::body::bytes())
-        .map(|body_bytes: bytes::Bytes| {
-            format!("{}\n", std::str::from_utf8(body_bytes.as_ref()).unwrap())
-        });
+    // run it
+    let addr = "0.0.0.0:8080";
+    let tcp_listener = TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", addr);
+    axum::Server::from_tcp(tcp_listener.into_std().unwrap())
+        .unwrap()
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
-    let routes = help.or(echo);
-    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await
+async fn help() -> &'static str {
+    "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`\n"
+}
+
+async fn echo(mut stream: BodyStream) -> Bytes {
+    if let Some(Ok(s)) = stream.next().await {
+        s
+    } else {
+        Bytes::new()
+    }
 }
